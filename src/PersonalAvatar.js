5f075748a1bf4d642fe12458fa23b5553c69f0a7
Jason Jones <jason.e.jones@gmail.com>
2021-08-05 22:18:13 -0400

    Account PersonalAvatar Auth

---
+++ src/PersonalAvatar.js
@@ -0,0 +1,126 @@
+import { SupabaseAuthClient } from '@supabase/supabase-js/dist/main/lib/SupabaseAuthClient';
+
+export default function PersonalAvatar({ url, onUpload }) {
+  const [avatarUrl, setAvatarUrl] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    if (url) downladImage(url);
+  }, [url]);
+
+  async function downladImage(path) {
+    try {
+      const { data, error } = await supabase.storage
+        .from('avatars')
+        .download(path);
+      if (error) {
+        throw error;
+      }
+      const url = URL.createObjectURL(data);
+      setAvatarUrl(url);
+    } catch (error) {
+      console.log('Error downloading image', error.message);
+    }
+  }
+
+  async function uploadAvatar(event) {
+    try {
+      setUploading(true);
+
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error('You must select an image to upload');
+      }
+
+      const file = event.target.files[0];
+      const fileExt = file.name.split('.').pop();
+      const fileName = `${Math.random()}.${fileExt}`;
+      const filePath = `${fileName}`;
+
+      let { error: uploadError } = await supabase.storage
+        .from('avatars')
+        .upload(filePath, file);
+
+      if (uploadError) {
+        throw uploadError;
+      }
+
+      onUpload(filePath);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setUploading(false);
+    }
+  }
+
+  return (
+    <div>
+      {avatarUrl ? (
+        <Avatar
+          size={'2xl'}
+          src={avatarUrl}
+          alt='Avatar'
+          mb={4}
+          pos={'relative'}
+          _after={{
+            content: '""',
+            w: 4,
+            h: 4,
+            bg: 'green.300',
+            border: '2px solid white',
+            rounded: 'full',
+            pos: 'absolute',
+            bottom: 0,
+            right: 3,
+          }}
+        />
+      ) : (
+        <Avatar
+          size={'2xl'}
+          src={avatarUrl}
+          alt='Avatar'
+          mb={4}
+          pos={'relative'}
+          _after={{
+            content: '""',
+            w: 4,
+            h: 4,
+            bg: 'green.300',
+            border: '2px solid white',
+            rounded: 'full',
+            pos: 'absolute',
+            bottom: 0,
+            right: 3,
+          }}
+        />
+      )}
+      <div>
+        <Button
+          size='sm'
+          flex={1}
+          mb={4}
+          fontSize={'sm'}
+          rounded={'full'}
+          _focus={{
+            bg: 'gray.200',
+          }}
+        >
+          <label className='button primary block' htmlFor='single'>
+            {uploading ? 'Uploading ...' : 'Upload'}
+          </label>
+        </Button>
+
+        <input
+          style={{
+            visibility: 'hidden',
+            position: 'absolute',
+          }}
+          type='file'
+          id='single'
+          accept='image/*'
+          onChange={uploadAvatar}
+          disabled={uploading}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+// initialize the supabase client
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const supacase = createClient(supabaseUrl, supabaseAnonKey);
